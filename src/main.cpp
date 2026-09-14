4c040dc195c612ea15ac90434f04d1886a960f11
yy981-jp <134594870+yy981-jp@users.noreply.github.com>
2026-09-14 21:18:17 +0900

    feat: dir scanner

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <filesystem>
 #include <string>
+#include <iostream>
 #include <vector>
 #include <stack>
 #include <cstdint>
@@ -9,21 +10,63 @@ namespace fs = std::filesystem;
 // disable_recursion_pending
 
 
-using EntryId = uint64_t;
+struct EntryId {
+	uint64_t value = 0;
+
+	static EntryId INVALID() { return {UINT64_MAX}; }
+	bool isValid() { return value != INVALID().value; }
+
+	auto operator<=>(const EntryId&) const = default;
+	EntryId operator++(int) {
+		EntryId b = *this;
+		value++;
+		return b;
+	}
+};
 
 struct Entry {
 	EntryId id;
+	EntryId parent;
 	std::string name;
 };
 
-
-int main() {
-	fs::path cd = fs::current_path();
-
+struct Ctx {
 	std::vector<Entry> entries;
 	std::stack<EntryId> parents;
-	int lastNest = 0;
-	for (const auto& e: fs::recursive_directory_iterator(cd)) {
+	int depth = 0;
+	EntryId cid{};
+};
 
+void dir(Ctx& ctx, const fs::path& targetDir) {
+	for (const auto& e: fs::directory_iterator(targetDir)) {
+		Entry ent{
+			.id = ctx.cid++,
+			.parent = ctx.parents.empty()? EntryId::INVALID() : ctx.parents.top(),
+			.name = e.path().filename().string(),
+		};
+		ctx.entries.push_back(ent);
+
+		if (e.is_directory()) {
+			ctx.parents.push(ent.id);
+			dir(ctx, e.path());
+			ctx.parents.pop();
+		}
+
+
+	}
+}
+
+
+int main(int argc, char *argv[]) {
+	fs::path target;
+	if (argc < 2) target = fs::current_path();
+	else target = fs::path(argv[1]);
+
+
+	Ctx ctx{};
+	dir(ctx, target);
+
+	for (const auto& e: ctx.entries) {
+		printf("%llu-%llu:\t%s\n", e.id.value, e.parent.value, e.name.c_str());
 	}
 }
